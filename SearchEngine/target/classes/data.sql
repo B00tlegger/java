ed6938bbefe9de3c71047156a1c318215a834d12
Булгаков Андрей Анатольевич <88447317+B00tlegger@users.noreply.github.com>
2021-12-22 16:02:04 +0300

    Delete SearchEngine/target/classes directory

--- SearchEngine/target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO field (id, name, selector, weight) values (1, "head", "head", 1.0) on duplicate key update name = "head";
-INSERT INTO field (id, name, selector, weight) values (2, "body", "body", 0.8) on duplicate key update name = "body";
